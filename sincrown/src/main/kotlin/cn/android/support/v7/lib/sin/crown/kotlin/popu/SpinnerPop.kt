33b874d5924253f953f23fec7987978c683af909
梦影雾花 <pengzhimingcn@163.com>
2018-07-06 16:06:09 +0800

    SpinnerPop上下左右容器，扩展。

--- sincrown/src/main/kotlin/cn/android/support/v7/lib/sin/crown/kotlin/popu/SpinnerPop.kt
+++ sincrown/src/main/kotlin/cn/android/support/v7/lib/sin/crown/kotlin/popu/SpinnerPop.kt
@@ -3,11 +3,15 @@ package cn.android.support.v7.lib.sin.crown.kotlin.popu
 import android.content.Context
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.View
 import android.view.View.OVER_SCROLL_NEVER
 import android.view.ViewGroup
 import android.widget.PopupWindow
+import android.widget.RelativeLayout
 import cn.android.support.v7.lib.sin.crown.R
+import cn.android.support.v7.lib.sin.crown.kotlin.common.px
+import cn.android.support.v7.lib.sin.crown.kotlin.widget.RoundRelativeLayout
 import cn.android.support.v7.lib.sin.crown.utils.PopuWindowUtils
 import org.jetbrains.anko.*
 import org.jetbrains.anko.recyclerview.v7.recyclerView
@@ -16,37 +20,155 @@ import org.jetbrains.anko.sdk25.coroutines.onClick
 //        //数据
 //        var list = mutableListOf<String>()
 //        var sp = SpinnerPop(this, list)
-//        //创建item视图,参数为下标position
+//        //创建recyclerView内部item视图,参数为下标position
 //        sp.onCreateView {
-//            UI { }.view
+//            UI { }.view //注意最好手动配置item的宽度和高度。
 //        }
 //        //视图刷新[业务逻辑都在这处理]，返回 视图itemView和下标postion
 //        sp.onBindView { itemView, position -> }
+//        //显示
+//        sp.showAsDropDown(this@apply,50,-50)
+
+//fixme 以下视图，根据需求可随意扩展。上下左右，四个容器只要不为空，就都会显示出来。centerView在四个容器的中间。
+//fixme 记得手动配置各个容器的宽和高。默认都是wrapContent，如果没有内容。就等于是空，是不会显示出来的。
+
+//                            sp.topView?.apply {//上方容器}
+//                            sp.leftView?.apply {//左边容器}
+//                            sp.rightView?.apply {//右边容器}
+//                            sp.bottomView?.apply {//底部容器}
+//                            sp.centerView?.apply {//中间容器，recyclerView的外框容器 }
+//                            sp.containerView?.apply { //最外层容器，囊括以上所有布局。}
 
 //传入的list和原有list是同一个对象，已经绑定。只要不重新赋值=，就会一直相互影响。
-class SpinnerPop(var context: Context, var list: MutableList<*>) {
+//styleAnime动画文件
+class SpinnerPop(var context: Context, var list: MutableList<*>, var styleAnime: Int = R.style.crown_window_alpha_scale_drop) {
     var pop: PopupWindow? = null
     var recyclerView: RecyclerView? = null
-    var contentView: View? = null//最外层组件容器
+
+    //fixme 最外层容器。
+    var containerView: RoundRelativeLayout? = null
+
+    //fixme 左边容器，recyclerView正左边
+    var leftView: RoundRelativeLayout? = null
+
+    //fixme 上方容器，recyclerView正上面
+    var topView: RoundRelativeLayout? = null
+
+    //fixme 中间容器，recyclerView的外框容器,即可以当作外层控件使用。随意更改样式
+    //fixme 上下左右，四个容器只要不为空，就都会显示出来。centerView在四个容器的中间。
+    var centerView: RoundRelativeLayout? = null
+
+    //fixme 下方容器，recyclerView正下面
+    var bottomView: RoundRelativeLayout? = null
+
+    //fixme 左边容器，recyclerView正右边
+    var rightView: RoundRelativeLayout? = null
 
     //fixme 创建itemView视图 [需要自动手动实现]
     fun onCreateView(itemView: (positon: Int) -> View) {
-        contentView = context.UI {
+        var view = context.UI {
             verticalLayout {
                 var layoutParams = ViewGroup.LayoutParams(wrapContent, matchParent)
                 setLayoutParams(layoutParams)
-                recyclerView = recyclerView {
-                    var linearLayoutManager = LinearLayoutManager(context)
-                    linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
-                    layoutManager = linearLayoutManager
-                    adapter = MyAdapter(this@SpinnerPop, itemView)
 
-                    setOverScrollMode(OVER_SCROLL_NEVER);//设置滑动到边缘时无效果模式
-                    setVerticalScrollBarEnabled(false);//滚动条隐藏
-
-                }.lparams {
+                //外层容器
+                containerView = RoundRelativeLayout(context).lparams {
                     width = wrapContent
                     height = wrapContent
+                }
+                addView(containerView)
+                containerView?.apply {
+                    relativeLayout {
+                        var layoutParams = RelativeLayout.LayoutParams(wrapContent, wrapContent)
+                        setLayoutParams(layoutParams)
+
+                        //左边容器
+                        leftView = RoundRelativeLayout(context)?.apply {
+                            id = px.id("spinnerPop_leftView")
+                        }?.lparams {
+                            width = wrapContent
+                            height = wrapContent
+                            alignParentLeft()
+                            bottomOf(px.id("spinnerPop_topView"))
+                        }
+                        addView(leftView)
+
+                        //上方容器
+                        topView = RoundRelativeLayout(context)?.apply {
+                            id = px.id("spinnerPop_topView")
+                        }?.lparams {
+                            width = wrapContent
+                            height = wrapContent
+                            alignParentTop()
+                            leftView?.let {
+                                rightOf(it)
+                            }
+                        }
+                        addView(topView)
+
+                        //中间容器，recyclerView容器
+                        centerView = RoundRelativeLayout(context)
+                        addView(centerView)
+                        centerView?.apply {
+                            id = px.id("spinnerPop_centerView")
+                            frameLayout {
+                                var layoutParams = ViewGroup.LayoutParams(wrapContent, wrapContent)
+                                setLayoutParams(layoutParams)
+                                recyclerView = recyclerView {
+                                    var linearLayoutManager = LinearLayoutManager(context)
+                                    linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
+                                    layoutManager = linearLayoutManager
+                                    adapter = MyAdapter(this@SpinnerPop, itemView)
+
+                                    setOverScrollMode(OVER_SCROLL_NEVER);//设置滑动到边缘时无效果模式
+                                    setVerticalScrollBarEnabled(false);//滚动条隐藏
+
+                                }.lparams {
+                                    width = wrapContent
+                                    height = wrapContent
+                                }
+                            }
+                        }?.lparams {
+                            width = wrapContent
+                            height = wrapContent
+                            topView?.let {
+                                bottomOf(it)
+                            }
+                            leftView?.let {
+                                rightOf(it)
+                            }
+                            above(px.id("spinnerPop_bottomView"))
+                        }
+
+                        //下方容器
+                        bottomView = RoundRelativeLayout(context)?.apply {
+                            id = px.id("spinnerPop_bottomView")
+                        }?.lparams {
+                            width = wrapContent
+                            height = wrapContent
+                            alignParentBottom()
+                            leftView?.let {
+                                rightOf(it)
+                            }
+                        }
+                        addView(bottomView)
+
+                        //右边容器
+                        rightView = RoundRelativeLayout(context)?.apply {
+                            id = px.id("spinnerPop_rightView")
+                        }?.lparams {
+                            width = wrapContent
+                            height = wrapContent
+                            centerView?.let {
+                                rightOf(it)
+                            }
+                            topView?.let {
+                                bottomOf(it)
+                            }
+                        }
+                        addView(rightView)
+
+                    }
                 }
                 //fixme 填充popuwindow的底部
                 view {
@@ -60,7 +182,7 @@ class SpinnerPop(var context: Context, var list: MutableList<*>) {
                 }
             }
         }.view
-        pop = PopuWindowUtils.getInstance().showPopuWindow(contentView, R.style.crown_window_alpha_scale_drop)
+        pop = PopuWindowUtils.getInstance().showPopuWindow(view, styleAnime)
     }
 
     var onBindView: ((itemView: View, position: Int) -> Unit)? = null
